0a5b29333ed62b10799dec5df93341919a1c5846
olivier marin <oliviermarin92@gmail.com>
2015-08-19 21:31:13 +0200

    #COMMIT : modify while cvs loop
    #COMMIT : modify while cvs loop

--- while-csv-loop/z-csv-while-loop/src/csv/loop/service/MainService.java
+++ while-csv-loop/z-csv-while-loop/src/csv/loop/service/MainService.java
@@ -5,7 +5,7 @@ import csv.loop.domain.Person;
 
 public class MainService {
 	
-	private final static String filePath = "C:/Users/F78973/Desktop/person.csv";
+	private final static String filePath = "path/rapport-stage-exemples/while-csv-loop/file/person.csv";
 	
 	public static void main(String[] args) {
 		
